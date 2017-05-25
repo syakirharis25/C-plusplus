a2dc4b1dd44802d13fdc02eb6bbb894f0094a9f8
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2017-05-25 23:25:50 +0800

    add c++ comments codes

---
+++ comments.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=5;
+    int b=a;
+    a=a+10;
+
+    cout << "Value of a: " << a << " Value of b: " << b;
+
+    b = b + 20;
+
+    /*This is
+    a multi-line
+    comment*/
+
+    //This is a single line comment
+    return 0;
+}
