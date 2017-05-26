fe3d66150f92bb93084c87e5e1d87367a684f79d
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2017-05-27 05:25:39 +0800

    add c++ data types

---
+++ data_types.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    bool mybool = false;
+    char mychar = 'A';
+    int myint = 15;
+    float myfloat = 25.63;
+
+    cout <<"Value in mybool: "<<mybool<<endl;
+    cout <<"Value in mychar: "<<mychar<<endl;
+    cout <<"Value in myint: "<<myint<<endl;
+    cout <<"Value in myfloat: "<<myfloat;
+
+    return 0;
+}
