9ed342cf081d54681f9ce9d11813aee9d0463b62
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2017-05-27 05:02:48 +0800

    add new line

---
+++ newline.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Universe!" << "\n";
+    cout << "Hello World!" << endl;
+    cout << "Hello Youtube!";
+
+    return 0;
+}
