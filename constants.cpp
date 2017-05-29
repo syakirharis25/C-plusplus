6c90d219c22dd0b31b255588d2236a212293aafa
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2017-05-30 02:16:10 +0800

    add c++ constants

---
+++ constants.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+#define PI 3.14
+
+int main()
+{
+    //const float PI=3.14;
+    float radius=5;
+    cout<<"Area of a circle with radius "<<radius<<" is: "<<PI*radius*radius;
+    return 0;
+}
