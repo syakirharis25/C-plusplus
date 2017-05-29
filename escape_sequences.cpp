5abcdb768be2d57875a3c43d6317f6786bfc6b38
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2017-05-30 02:25:57 +0800

    add c++ escape sequences

---
+++ escape_sequences.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int var1=10, var2=20;
+    cout<<"\a";
+    cout<<"Value of var1:\t"<<var1<<"\n";
+    cout<<"Value of var2:\t"<<var2;
+    return 0;
+}
