104ce9d17af7d49a018db9babf5e658461ba0eba
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2017-05-30 03:29:16 +0800

    add c++ logical operators

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    x=100; y=20;
+    cout<<"Addition: "<<x+y<<endl;
+    cout<<"Subtraction: "<<x-y<<endl;
+    cout<<"Multiplication: "<<x*y<<endl;
+    cout<<"Division: "<<x/y<<endl;
+    cout<<"Remainder on division: "<<x%y;
+    return 0;
+}
---
+++ logical_operators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool x,y;
+    x=true; y=false;
+    cout<<"Logical AND Operator: "<<(x&&y)<<endl;
+    cout<<"Logical OR Operator: "<<(x||y)<<endl;
+    cout<<"Logical NOT Operator: "<<(!x);
+    return 0;
+}
